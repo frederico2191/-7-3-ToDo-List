392461f592c60801018d0b5bd57cce2768ed2f2f
Frederico Rodrigues <frederico2191@gmail.com>
2023-02-28 20:29:03 +0000

    End of Class

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,27 +1,47 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import ListItem from './ListItem.jsx'
 
 function List() {
-  const [toDos,setToDos] = useState([ { key: task:'clean dishes' } ])
-
-  const submitHandler = () => {
-    
-
+  const [toDos, setToDos] = useState([ {key: '', task:'clean dishes' } ])
+  const myInput = useRef();
+  
+  const handleButtonClick = () => {
+    setToDos([...toDos, {task: myInput.current.value}])
   }
-
-
-
-
+  
   return (
     <div>
-      <button type="submit" onClick={submitHandler}></button>
-       {!toDos.length ? <h5>No tasks, add a task</h5> : (
-        toDos.map((listElement, index)=>(
-          <ListItem key={index} task={listElement.task}/>
-        ))
-       )} 
+    <input ref={myInput}></input>
+
+    <button onClick={handleButtonClick}> Add a Task</button>
+    {console.log("toDos",toDos)}
+
+    {!toDos.length ? <h5>No tasks, add a task</h5> : (
+    toDos.map((listElement, index)=>(
+      <ListItem key={index} task={listElement.task}/>
+    ))
+    )} 
     </div>
   )
 }
 
-export default List
+export default List;
+
+
+
+// import React, { useState, useRef } from 'react'
+
+// export default function UnControlledInput() {
+//     const [myText,setMyText] = useState('');
+//     const myInput = useRef();
+//     console.log("uncontrolled: render")
+//   return (<>
+
+
+//         (<h1>Uncontrolled input</h1>)
+//         <input ref={myInput}></input>
+//         <button onClick={()=>{setMyText(myInput.current.value)}}></button>
+//         (<h2>{myText}</h2>)
+    
+//     </>)
+// }
